a419dd42838ebd5589756dce0a0b5000b46b8f87
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-01 15:52:14 +0100

    aula11wile

--- README.md
+++
@@ -1,3 +0,0 @@
-# programarEmC
-
-//https://www.youtube.com/watch?v=Jg6nxmvun30&list=PLa75BYTPDNKZWYypgOFEsX3H2Mg-SzuLW&index=12
---
+++ aula11_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    int contador = 10;
+
+    while (contador >= 5) {
+        printf("%i \n", contador);
+        --contador;
+    }
+
+    return 0;
+
+
+}
