ae35d9448b118ca24a07c37edc35292f455c4058
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-02 18:29:32 +0100

    aula18_exercicios_inverterSequenciaDosNumeros

---
+++ aula18_exercicios.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//inverter um numero sequencial digitado pelo usuário
+
+int main() {
+
+    int numero, cifra;
+
+    printf("Digite uma sequencia de números positivos:  ");
+    scanf("%i", &numero);
+
+    if (numero >= 0) {
+        do {
+            cifra = numero % 10;
+            printf("%i", cifra);
+            numero =  numero / 10;
+        } while (numero != 0);
+        printf("\n");
+
+    } else {
+        printf("nun");
+
+    }
+
+
+
+
+    return 0;
+}
