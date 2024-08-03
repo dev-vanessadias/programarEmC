fc60d5ef52e29176f7aeccf8a846e7df69536ac5
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-03 19:25:12 +0100

    aula25-matrizes

---
+++ aula25_matrizes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int matriz [3] [3] = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    printf("%i", matriz[0] [5]); // neste exemplo o primeiro quadro com valor 0 esta acessando a linha e o quadro 5 esta acessando a coluna
+
+}
