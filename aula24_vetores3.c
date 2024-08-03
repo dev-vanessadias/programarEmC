6d07ac6d1cb9730660632d30f728066e8dc3d89e
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-03 18:58:11 +0100

    aula24_vetores3

---
+++ aula24_vetores3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+    float notas[5] = {0};
+    float total = 0;
+    float media = 0;
+
+    printf("Insira 5 notas: \n");
+
+    for(int i = 0; i < 5; ++i)
+        scanf("%f", &notas[i]);
+
+    for(int i = 0; i < 5; ++i)
+    total += notas[i];
+
+    printf("A soma das notas é: %f \n", total);
+
+    media = total / 5;
+    printf("A média do aluno é: %f", media);
+
+
+
+
+}
