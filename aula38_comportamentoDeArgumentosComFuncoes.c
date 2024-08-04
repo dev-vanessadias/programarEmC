71820ce48fdd0eec45c35c7d15600bcc20f7a72f
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-04 22:53:17 +0100

    aula38_comportamentoDeArgumentosComFuncoes

---
+++ aula38_comportamentoDeArgumentosComFuncoes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    void funcaoPrint(int x, int vetor[]);
+    int x = 10;
+    int vetor[3] = {10};
+
+    funcaoPrint(x, vetor);
+
+    printf("Variavel int na funcao principal = %i \n", x);
+    printf("Vetor na funcao principal = %i \n", vetor[1]);
+
+    return 0;
+}
+void funcaoPrint(int x, int vetor[]) {
+
+    x = x + 10;
+    vetor[1] = 20;
+
+    printf("VARIAVEL INT NA FUNCAO PRINT = %i \n", x);
+    printf("VETOR NA FUNCAO PRINT = %i \n \n", vetor[1]);
+
+}
