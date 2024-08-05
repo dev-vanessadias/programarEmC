3f05eefca7ecb88c47d9545726808702e9d86140
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-05 11:05:26 +0100

    aula42_passaEreceberEstruturas

--- aula42_pasarEreceberEstruturas.c
+++ aula42_pasarEreceberEstruturas.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 
+
+struct horario {
+    int horas;
+    int minutos;
+    int segundos;
+};
+
 int main(void) {
 
+    struct horario teste(struct horario x); // chamada da funcao 1
+
+    struct horario agora;
+    agora.horas = 10;
+    agora.minutos = 42;
+    agora.segundos = 58;
+
+    struct horario proxima;
+    proxima = teste(agora);
+
+    printf("Na main - proxima:  %i:%i:%i", proxima.horas,
+                                           proxima.minutos,
+                                           proxima.segundos);
+
+    return 0;
+}
+struct horario teste(struct horario x) { // funcao 1
+
+    printf("Na funcao 1 - teste: %i:%i:%i", x.horas,
+                                            x.minutos,
+                                            x.segundos);
+
+    x.horas = 100;
+    x.minutos = 100;
+    x.segundos = 100;
+    printf("\n");
+
+    return x;
 }
