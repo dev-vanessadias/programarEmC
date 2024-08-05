0da80871a187ac4be6cca011ea8227d0a3ad4459
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-05 12:27:17 +0100

    aula43_inicializandoEstruturas.c

--- aula43_inicializandoEstruturas.c
+++ aula43_inicializandoEstruturas.c
@@ -2,4 +2,16 @@
 
 int main(void) {
 
+    struct horario {
+        int horas;
+        int minutos;
+        int segundos;
+    }agora = {10, 20, 30}; //definiu a variavel e atribuiu
+                                                 //a estrutura em uma mesma linha.
+
+    printf("Na main - proxima:  %i:%i:%i \n", agora.horas,
+                                              agora.minutos,
+                                              agora.segundos);
+
+    return 0;
 }
