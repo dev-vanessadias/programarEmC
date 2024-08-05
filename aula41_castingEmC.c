899c90f8c2c452b6336367318f87ad6c81996dac
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-05 09:54:19 +0100

    aula41_castingEmC

---
+++ aula41_castingEmC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int x = 16;
+    int y = 3;
+
+    float resultado = (float) x / y; //O casting é essa informacao
+    // entre parenteses (float) indicando o tipo de retorno da funcao
+
+
+    printf("%f \n", resultado);
+
+}
