ca6630e5d7ae44e446d62263022787e904b23545
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-05 14:51:12 +0100

    aula44_vetoresDeEstruturas

--- aula44_vetoresDeEstruturas.c
+++ aula44_vetoresDeEstruturas.c
@@ -2,4 +2,30 @@
 
 int main(void) {
 
+    struct horario {
+        int horas;
+        int minutos;
+        int segundos;
+    };
+
+    struct horario teste [5] =
+    {
+        {10, 20, 30},
+        {20, 30, 40},
+        {30, 40,50},
+        {40, 50, 60},
+        {50, 60, 70},
+    };
+
+    int i;
+    for (i = 0; i < 5; ++i) {
+
+        printf("%i:%i:%i \n", teste[i].horas,
+                               teste[i].minutos,
+                               teste[i].segundos);
+
+    }
+
+    return 0;
+
 }
