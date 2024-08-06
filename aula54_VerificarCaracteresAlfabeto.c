326bf764c207dbbf8741f69c9a8b41492b8ed95f
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-06 16:27:57 +0100

    aula54_verificarCaracteresAlfabeto

--- aula54_VerificarCaracteresAlfabeto.c
+++ aula54_VerificarCaracteresAlfabeto.c
@@ -2,4 +2,25 @@
 
 int main(void) {
 
+void alfabetico(char variavel); //declaracao da funcao 1 na main
+
+char nome[20];
+printf("Digite uma palavra: \n");
+    scanf("%s", &nome);
+
+    int i = 0;
+    while (nome[i] != '\0') {
+        alfabetico(nome[i]);
+        ++i;
+    }
+
+    return 0;
+}
+void alfabetico(char variavel) { //funcao 1
+    if((variavel >= 'a' && variavel <= 'z') ||
+       (variavel >= 'A' && variavel <= 'Z')) {
+        printf("É UMA LETRA \n");
+    } else {
+        printf("NÃO É UMA LETRA \n");
+    }
 }
