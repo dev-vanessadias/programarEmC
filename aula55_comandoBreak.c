c6b38edc5249715c05ad37991b0ad48177ad2542
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-06 17:15:35 +0100

    aula55_comandoBreak

--- aula55_comandoBreak.c
+++ aula55_comandoBreak.c
@@ -2,4 +2,15 @@
 
 int main(void) {
 
+    int i ;
+    for (i = 1; i <= 20; ++i) {
+        if (i % 3 == 0 && i % 9 == 0) {
+            printf(" o numero %i é divisivel \n", i);
+            break;// para o codigo quando encontra a primeira condicao verdadeira da logica  pedida
+        } else {
+            printf("o numero %i náo é \n", i);
+        }
+    }
+    getchar();
+    return 0;
 }
