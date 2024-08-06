a6783a3c8c456b8bb6e7a914fe36998b10c54278
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-06 13:54:37 +0100

    aula49_contarString

--- aula49_contarCaracteresDeString.c
+++ aula49_contarCaracteresDeString.c
@@ -2,4 +2,29 @@
 
 int main(void) {
 
+    int tamanhoString(char string[]); // declaracao da funcao 1 na main
+    char stringUsauario[20];
+
+    printf("Digite uma palavra(string): \n ");
+    scanf("%s", stringUsauario);
+
+    int num = tamanhoString(stringUsauario); // chamada da funcao 1
+
+    printf("A string %s possui %i caracteres. \n", stringUsauario,
+                                                   num);
+
+    return 0;
+
 }
+int tamanhoString(char string[]) { // funcao 1
+
+    int numCaracteres = 0;
+
+    while(string[numCaracteres] != '\0') {
+        ++numCaracteres; //conta numero de letras
+    }
+    ++numCaracteres; //conta numero de caracteres
+
+    return numCaracteres;
+
+};
