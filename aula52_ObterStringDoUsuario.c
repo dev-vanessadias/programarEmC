f2db8134d05fde3d68ce924e9859cd527391d3b8
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-06 15:21:21 +0100

    aula52_obterStringDoUsuario

--- aula52_ObterStringDoUsuario.c
+++ aula52_ObterStringDoUsuario.c
@@ -2,4 +2,15 @@
 
 int main(void) {
 
+    char nome[20];
+    char sobrenome[20];
+
+    printf("Insira o seu nome: \n");
+    scanf("%s", &nome);
+    printf("Insira o seu sobrenome: \n");
+    scanf("%s", &sobrenome);
+
+    printf("O seu nome completo é: %s %s \n", nome, sobrenome);
+
+    return 0;
 }
