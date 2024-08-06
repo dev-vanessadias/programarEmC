ef3f3048ee5237069be2f3e6ef3ca22979592902
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-06 17:20:57 +0100

    aula56_funcaoSystem

--- aula56_funcaoSystem.c
+++ aula56_funcaoSystem.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
+    printf("www.facebook.com/deAlunoPraAluno");
+
+    system("pause"); // usado apenas para dev C++ e windows
+
+    return 0;
 }
