636c201dca0eb78266f10b9f85ea684bf125fc84
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-07 19:51:24 +0100

    aula70_prePosIncrementoDecremento

--- aula70_PrePosIncrementoDecremento.c
+++ aula70_PrePosIncrementoDecremento.c
@@ -2,6 +2,22 @@
 
 int main(void) {
 
+    int x = 1;
+    int y = 0;
+
+    //PRE INCREMENTO ou decremento
+    y = ++x; // AQUI Y PASSA A VALER 2 PQ X=1 +  X=1
+    x = x + 1;
+    y = x;
+
+    //POS INCREMENTO ou decremento
+    y = x++;
+    y = x;
+    x = x + 1;
+
+
+    printf("x = %i \n", x);
+    printf("x = %i \n", y);
 
     getchar();
     return 0;
