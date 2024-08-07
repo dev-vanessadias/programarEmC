5307095e5ecfdc79d351c1da5142ce63169c6359
Macbook Air 13-VanessaDias <macbookair13@Air-de-Macbook.lan>
2024-08-07 11:31:00 +0100

    aula60_teoriaPonteiros

--- aula60_introducaoPonteiros.c
+++ aula60_introducaoPonteiros.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
+ //aula teorica
+ //https://www.youtube.com/watch?v=r7f-aR7vgg0&list=PLa75BYTPDNKZWYypgOFEsX3H2Mg-SzuLW&index=61
 
-    getchar();
     return 0;
 }
