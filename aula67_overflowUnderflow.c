07b25cf55d26b70159a261e20024b5c40f8fb401
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-07 17:34:56 +0100

    aula67_overflowUnderflow

--- aula67_overflowUnderflow.c
+++ aula67_overflowUnderflow.c
@@ -2,6 +2,15 @@
 
 int main(void) {
 
+    short x = 2146637237; //overflow ou transbordamento da variavel -  memoria nao possui bits o suficiente para armazenar a variavel que é muito grande
+    short y = -62616261323;
+
+    short x = 2146637237; // underflow - uma variavel declarada como int consegue armazenar um numero maior que uma short
+    short y = -62616261323;
+
+    printf("%i \n", x);
+    printf("%i \n", y);
+
 
     getchar();
     return 0;
