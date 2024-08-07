e51a1f7eec676dc4425110434b8a42969b130c2d
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-07 15:23:13 +0100

    teste aula 63

--- aula64_ponteirosEstruturas2.c
+++ aula64_ponteirosEstruturas2.c
@@ -2,7 +2,27 @@
 
 int main(void) {
 
-\\
+    struct horario {
+        int hora;
+        int minuto;
+        int segundo;
+    };
+
+    struct horario agora, *depois;
+    depois = &agora;
+
+    // primeira forma opciobnal de declarar ponteiro
+    //(*depois).hora = 20;
+    //(*depois).minuto = 30;
+    //(*depois).segundo = 15;
+
+    // segunda forma opciobnal de declarar ponteiro
+    depois->hora = 20;
+    depois->minuto = 30;
+    depois->segundo = 16;
+
+    printf("%i:%i:%i", agora.hora, agora.minuto, agora.segundo);
+
     getchar();
     return 0;
 }
