c1b6533366a86806636ea86a470e885fdd7fc947
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 18:35:14 +0100

    aula83_maniplulacaoDeArquic=visTXT3

--- aula83_manipulacaoDeArquivosTXT3.c
+++ aula83_manipulacaoDeArquivosTXT3.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
-    getchar();
+    FILE *file;
+    file = fopen("string.txt", "r");
+
+    if(file == NULL) {
+        printf("Nao foi possivel encontrar o arquivo. \n");
+        getchar();
+        exit(0);
+    }
+
+    char frase[100];
+
+    while (fgets(frase, 100, file) != NULL) {
+        printf("%s", frase);
+    }
+    fclose(file);
     return 0;
 }
