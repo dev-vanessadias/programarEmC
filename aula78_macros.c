4dc93c63e94fe1900e7cbeecb15708513bb0e097
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 16:03:39 +0100

    aula78_macros

--- aula78_macros.c
+++ aula78_macros.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#define PI 3.14159
+#define DOIS_PI 2 * PI
+#define AREA_CIRCULO(raio) raio * raio * PI
+
 int main(void) {
+
+    int area = 10;
+;
+    printf("%f \n", PI);
+    printf("%f \n", DOIS_PI);
+    printf("%f \n", AREA_CIRCULO(area));
 
     getchar();
     return 0;
