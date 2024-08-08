1c995d638fb68d57bb29704f3cad95dc3e08b1ca
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 17:55:53 +0100

    aula81_manipulacaoDeArquivoTXTescreverDados

--- aula81_manipulcaoDeArquivosTXT.c
+++ aula81_manipulcaoDeArquivosTXT.c
@@ -2,6 +2,12 @@
 
 int main(void) {
 
+    FILE *file;
+    file =fopen("teste.txt", "w"); //essa acao cria um arquivo no diretorio aonde esta esque arquivo c
+
+    fprintf(file, "Pao com manteiga"); //escreve algo no arquivo txt criado
+    fclose(file); // essa acao salva o arquivo
+
     getchar();
     return 0;
 }
