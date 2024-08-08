daae9e1921b0bf1270844c5f5afcb671c46e86c4
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 13:21:40 +0100

    aula73_ponteirosVetoresFuncoes

--- aula73_ponteirosVetoresFuncoes.c
+++ aula73_ponteirosVetoresFuncoes.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+int somarVetor(int vetor[], const int n) { // funcao 1 - vai somar os valores de um vetor utiluzando ponteiros
+
+    int soma = 0;
+    int *ponteiro;
+    int *const finalVetor = vetor + n; // o +n no final da expresao pede para o ponteiro apontar no ultimo membro
+                                       //do vetor, sem ele o ponteiro apontaria para o primeiro membro do vetor.
+
+    //ponteiro = busca endereco de memoria
+    //*ponteiro = busca o valor armazenado no endereco de memoria
+    for (ponteiro = vetor; ponteiro < finalVetor; ++ponteiro) {
+        soma += *ponteiro;
+    }
+    return soma; //retorno do laco for
+}
+
 int main(void) {
+    int somarVetor(int vetor[], const int n); //declaracao da funcao 1
+    int vetor[10] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}; //variavel com vetor que sera somado
+
+    printf("A soma dos membros do vetor = %i", somarVetor(vetor,10));
 
     getchar();
     return 0;
