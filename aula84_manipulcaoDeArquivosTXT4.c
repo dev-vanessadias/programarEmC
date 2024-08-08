bd437331fb0fdaaddcf89f3475ff5d4dae499efb
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 18:56:29 +0100

    aula84_manipulacaoDeArquivosTXT4

--- aula84_manipulcaoDeArquivosTXT4.c
+++ aula84_manipulcaoDeArquivosTXT4.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
-    getchar();
+    FILE *file;
+    file = fopen("string.txt", "a");
+
+    if (file ==NULL) {
+        printf("Arquivo não encontrado.");
+        getchar();
+        exit(1);
+    }
+
+    fprintf(file, "Primeita linha \n");
+
+    char frase[] = "Segunda linha \n";
+    fputc(frase, file);
+
+    char caractere = '3';
+    fputc(caractere, file);
+
+    fclose(file);
+
+
     return 0;
 }
