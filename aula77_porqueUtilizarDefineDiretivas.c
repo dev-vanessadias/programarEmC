b589eea84e7a2565832c4a51fa6e2660bb57ea8d
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 15:32:33 +0100

    aula77_porqueUsar#Define

--- aula77_porqueUtilizarDefineDiretivas.c
+++ aula77_porqueUtilizarDefineDiretivas.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+#define NUMERO_DE_ALUNOS 2000
+
+void umafuncao(void) {
+    printf("%i \n", NUMERO_DE_ALUNOS);
+}
+void umaOutraFuncao(void) {
+    printf("%i \n", NUMERO_DE_ALUNOS);
+}
 int main(void) {
+
+    void umafuncao(void);
+    void umaOutraFuncao(void);
+
+    printf("%i \n", NUMERO_DE_ALUNOS);
+    umafuncao();
+    umaOutraFuncao();
 
     getchar();
     return 0;
