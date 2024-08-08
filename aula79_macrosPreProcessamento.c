9bfa243a9facbfb97fa34e2536e3f4754155ed18
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 16:40:11 +0100

    aula79_macrosPreProcessamento

--- aula79_macrosPreProcessamento.c
+++ aula79_macrosPreProcessamento.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#define MAIOR(x, y) x > y ? x : y //x é mair que j? se sim retorne x se nao retorne y
+#define E_MINUSCULO(char) char >= 'a' && char <= 'c'
+
 int main(void) {
+
+    char x = 'c';
+
+    if(E_MINUSCULO(x)) {
+        printf("E uma letra minuscula \n");
+    } else {
+        printf("Nao é uma leta minuscula \n");
+    }
+
+    printf("%i \n", MAIOR(10, 50));
 
     getchar();
     return 0;
