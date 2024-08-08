176feeb6745efebd4713bf71bcdecb3a2a42813c
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 12:03:36 +0100

    aula72_ponteirosEveotres2

--- aula72_PonteirosVetores2.c
+++ aula72_PonteirosVetores2.c
@@ -2,6 +2,24 @@
 
 int main(void) {
 
+    int vetor[3] = {1, 2, 3};
+    //int *ponteiro = vetor; // o ponteiro vai apontar o endereco de memoria e nao precisa colocar o simbol &
+    //int *ponteiro = &vetor[1]; // nessa declaracao opcional o ponteiro é forcado a localizar o numero do vetor
+
+    int *ponteiro = &vetor[0];
+
+    //++ponteiro; // esta adicionando mais um e pulando o endereco de memoria.
+
+    *(ponteiro + 1) = 10; // altera o segundo valor do vetor de 2 para 10
+
+    //printf("%p \n", ponteiro);
+
+    //ponteiro = &vetor[1];
+    //printf("%p \n", ponteiro);
+
+    //ponteiro = &vetor[2];
+    //printf("%p \n", ponteiro);
+
     getchar();
     return 0;
 }
