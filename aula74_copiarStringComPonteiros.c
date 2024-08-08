469bce86d358640bd2e5b73dfdd00f4d3d29365a
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 13:46:21 +0100

    aula74_copiarStringComPonteiros

--- aula74_copiarStringComPonteiros.c
+++ aula74_copiarStringComPonteiros.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void copiarString(char *copiarDaqui, char *colarAqui) { //funcao global 1
+
+    while (*copiarDaqui != '\0') {
+        *colarAqui = *copiarDaqui;
+        ++copiarDaqui;
+        ++colarAqui;
+    }
+    *colarAqui = '\0';
+
+}
 int main(void) {
+
+    void copiarString(char *copiarDaqui, char *colarAqui); // chamada da funcao global 1
+
+    char string1[] = "Pao com manteiga";
+    char string2[20];
+
+    copiarString(string1, string2);
+    printf("%s \n", string2);
 
     getchar();
     return 0;
