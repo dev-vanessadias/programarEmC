80dc9580f0a0c861c5eef85f6856b412636424c1
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 14:13:25 +0100

    aula75_comando#definePreProcessamento1

--- aula75_comandoDefinePreProcessamento.c
+++ aula75_comandoDefinePreProcessamento.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+#define SIM 100
+#define NAO -100
+
+//acebook.com/DeAlunoParaAluno
+
 int main(void) {
+
+    int x;
+    printf("Digite um numero inteiro:");
+    scanf("%i, &x");
+
+    if(x <= 10) {
+        printf("%i", NAO);
+    } else {
+        printf("%i", SIM);
+    }
 
     getchar();
     return 0;
