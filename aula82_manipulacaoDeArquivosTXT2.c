2725887e73157d88ae227759466e4bf9d975e663
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 18:14:30 +0100

    aula82_manipulacaoDeArquivoTXT2

--- aula82_manipulacaoDeArquivosTXT2.c
+++ aula82_manipulacaoDeArquivosTXT2.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
+
+    FILE *file;
+    file = fopen("numeros.txt", "r");
+
+    if (file == NULL) {
+        printf("Arquivo nao encontrado \n");
+    }
+
+    int x, y, z;
+
+    fscanf(file, "%i %i %i", &x, &y, &z);
+    printf("%i %i %i \n", x, y, z);
 
     getchar();
     return 0;
