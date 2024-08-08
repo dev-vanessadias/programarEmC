3be26c11d2dc68aafd9a6a4bcaaa899e8e164ade
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-08 15:24:14 +0100

    aula76_comandoDefineProcess2

--- aula76_ComandoDefinePreProcess2.c
+++ aula76_ComandoDefinePreProcess2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+#define PI 3.14159
+#define NULO 0
+
+double areaCirculo(double raio) { //1// FUNCAO 1
+    return raio * raio * PI;
+}
+
 int main(void) {
+
+    double areaCirculo(double raio); // chamada da funcao 1 na main
+    double r; //nessa variavel ira guardar o valor de raio
+    int i = 3;
+
+    while (i != NULO) {
+        printf("Digite o raio do circulo: ");
+        scanf("%lf", &r);
+        printf("A area do cirsulo é %lf \n", areaCirculo(r));
+        --i;
+    }
 
     getchar();
     return 0;
