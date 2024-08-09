07b54342f8a7557e01d65ade657c9d55858ecc2d
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-09 20:20:22 +0100

    aula93_funcoesGETCARePUTCHAR

--- aula93_funcoesGETCHARePUTCHAR.c
+++ aula93_funcoesGETCHARePUTCHAR.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+//getchar - guarda valor digitado
+//puchar - imprimi valor digitado
+
 int main(void) {
+
+    //exemplo 1//
+   // char x[10];
+
+  // int i = 0;
+   // while ((x[i]= getchar()) != '\n' && i < 8) {
+  //      ++i;
+  //  }
+
+   // x[++i] = '\0';
+  //  printf("%s", x);
+
+  //  return 0;
+
+  //EXEMPLO 2 - ESSE PROGRAMA VAI REPLICA TUDO O QUE FOR ESCRITO
+int i = 0;
+ char x;
+
+ while ((x = getchar()) != '\n') {
+  putchar(x);
+ }
 
     return 0;
 }
