2b783c76d1cbfddc0741c6cbe9830cf06207bb21
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-09 18:25:33 +0100

    aula89_funcoesFGETCeGETC

--- aula89_funcoesFGETCeGETC2.c
+++ aula89_funcoesFGETCeGETC2.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+//EOF-  end of file quer que o ciclo seja executado ate atingir o final do arquivo
+
 int main(void) {
 
 
+    char x[100];
+
+    FILE *file = fopen("teste.txt", "r");
+
+    int i = 0;
+    while ((x[i] = fgetc(file)) != EOF) {
+        ++i;
+    }
+    x[i] = '\0';
+
+    printf("%s", x);
+    
     return 0;
 }
