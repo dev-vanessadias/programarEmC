3ddf4ac70a4290eb17446e90d5c1e92cff3cfa97
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-09 18:06:10 +0100

    aula88_funcoesFGETCeGETC

--- aula88_funcoesfgetcEgetc.c
+++ aula88_funcoesfgetcEgetc.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+// fgetc é usado apenas como funcao
+// getc pode ser usado também com macros
+// as funcoes fgetc e getc fazem a mesma coisa,
+// mas a funcao fgetc nao pode ser usada como macro, apenas como funcao.
+
 int main(void) {
+
+    char x[100];
+
+    //x[0] = fgetc(stdin);
+    //printf("%c", x[0]);
+
+    int i = 0;
+    while ((x[i] = fgetc(stdin)) != '\n') {
+        ++i;
+    }
+
+    x[i] = '\0';
+
+    printf("%s",x);
 
     return 0;
 }
