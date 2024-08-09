059e6159c09043aa843c77680e535da416c8ee71
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-09 17:46:51 +0100

    aula87_funcoesFreopenEfgts

--- aula87_funcoesFreopenEfgsts2.c
+++ aula87_funcoesFreopenEfgsts2.c
@@ -2,5 +2,16 @@
 
 int main(void) {
 
+    char x[100];
+
+    FILE *file = fopen("teste.txt", "r");
+
+    fgets(x, 100, file);
+    printf("%s \n", x);
+
+    //fropen("teste2.txt", "r", file);
+    fgets(x, 100, file);
+    printf("%s \n", x);
+
     return 0;
 }
