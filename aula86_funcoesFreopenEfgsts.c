24f90902a7b20f9e61331cb896d7b390f224e94b
dev-vanessadias <dev.vanessadias@gmail.com>
2024-08-09 17:25:57 +0100

    aula86_funcoesFreopenEfsts

--- aula86_funcoesFreopenEfgsts.c
+++ aula86_funcoesFreopenEfgsts.c
@@ -2,5 +2,18 @@
 
 int main(void) {
 
+    char x[100];
+
+    fgets(x, 100, stdin);
+    printf("%s", x);
+
+    //stdin // é o modo de entrada de informacao, exemplo teclado
+
+    freopen("arquivo1.tx", "r", stdin);
+    fgets(x, 100, stdin);
+    printf("%s", x);
+
+
+
     return 0;
 }
